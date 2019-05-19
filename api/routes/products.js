6b4d49b9535be13ccee129ef42a0577aa2205b8f
Servet93 <servetseker93@gmail.com>
2019-05-19 23:54:53 +0300

    ok

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,14 +11,41 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling POST requests to /products'
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    })
+
+    product.save().then(result=>{
+        console.log(result)
+        res.status(201).json({
+            message: 'Handling POST requests to /products',
+            createdProduct: result
+        })
+    }).catch(err=>{
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
     })
 })
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId
-
+    Product.findById(id).exec().then(doc => {
+        console.log("From database " + doc)
+        if(doc){
+            res.status(200).json(doc)
+        }else{
+            res.status(404).json({message: 'No valid entry found for provided ID'})
+        }
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({error:err})
+    })
+    /*
     if(id === 'special'){
         res.status(200).json({
             message: 'You discovered the special ID',
@@ -26,6 +56,7 @@ router.get('/:productId', (req, res, next) => {
             message: 'You discovered the special ID'
         })   
     }
+    */
 })
 
 router.patch('/:productId', (req, res, next) => {
